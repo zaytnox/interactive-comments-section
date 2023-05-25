43852ff7dea98771b82950be2472089c7b0c1db0
Zaytnox <zaytnox@gmail.com>
2023-05-25 16:42:37 -0500

    remove import data

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './routes/App';
-import '../public/data/data.json';
 
 const root = createRoot(document.querySelector('.app'));
 root.render(<App />);
