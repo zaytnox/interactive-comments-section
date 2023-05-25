52d7c058ffd337c12992d0cb7811ae8dd7f18e06
Zaytnox <zaytnox@gmail.com>
2023-05-25 16:46:23 -0500

    add contexts provider

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Home } from '../pages/Home';
+import Home from '../pages/Home';
+import '../styles/style.css';
+import { UserContextProvider } from '../contexts/UserContext';
+import { ModalContextProvider } from '../contexts/ModalContext';
+import { CommentsContextProvider } from '../contexts/CommentsContext';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <ModalContextProvider>
+      <UserContextProvider>
+        <CommentsContextProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </BrowserRouter>
+        </CommentsContextProvider>
+      </UserContextProvider>
+    </ModalContextProvider>
   );
 };
 
